445f94e6600ec81ec363af1eef6c119142ecb2a9
NutThai <88415649+NutThai@users.noreply.github.com>
2024-03-13 02:31:57 +0700

    Update tool.go

--- Gunpla-Shop_backend/domain/entity/tool.go
+++ Gunpla-Shop_backend/domain/entity/tool.go
@@ -5,6 +5,7 @@ type Tool struct {
 	Images      []string `json:"images"`
 	Name        string   `json:"name"`
 	Type        string   `json:"type"`
+	Brand       string   `json:"brand"`
 	Price       int64    `json:"price"`
 	Stock       int      `json:"stock"`
 	Description string   `json:"description"`
